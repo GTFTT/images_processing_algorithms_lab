51947153cc5d33edf9d7c852210172e39763a944
GTFTT <FTowerT@outlook.com>
2023-02-20 23:17:32 +0200

    Sobel's filter set as default page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import 'antd/dist/reset.css';
 import './App.css';
 import {Menu} from "./Components/Menu/Menu";
 import {SobelFilterLibrary} from "./Pages/SobelFilterLibrary/SobelFilterLibrary";
+import {SobelFilter} from "./Pages/SobelFilter/SobelFilter";
 
 function App() {
-  const [selectedMenuItem, setSelectedMenuItem] = useState(0);
+  const [selectedMenuItem, setSelectedMenuItem] = useState(1);
   return (
     <div className="App">
       <Menu
@@ -18,7 +19,12 @@ function App() {
             id: 0,
             label: `Sobel's filter from library`,
             component: <SobelFilterLibrary />
-          }
+          },
+          {
+            id: 1,
+            label: `Sobel's filter`,
+            component: <SobelFilter />
+          },
         ]}
       />
     </div>
